4cd33ccecd141649956ee955ecd34dec00b31c5d
mulhamna <mulhamna@gmail.com>
2026-04-10 19:51:35 +0700

    Fix Version 0.2.0

--- bin/pkgmap.js
+++ bin/pkgmap.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander'
+import { createRequire } from 'module'
 import { run } from '../src/index.js'
+
+const require = createRequire(import.meta.url)
+const { version } = require('../package.json')
 
 program
   .name('pkgmap')
   .description('See everything installed on your machine')
-  .version('0.1.0')
+  .version(version)
   .option('-m, --manager <name>', 'scan only a specific package manager')
   .option('-s, --search <package>', 'search for a specific package')
   .option('-e, --export', 'export results to pkgmap-export.json')
